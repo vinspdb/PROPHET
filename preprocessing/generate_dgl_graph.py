e5715468f8d8536417c15f4e954ae5e09608478c
vinspdb <57073656+vinspdb@users.noreply.github.com>
2025-01-27 16:15:23 +0100

    Update generate_dgl_graph.py

--- preprocessing/generate_dgl_graph.py
+++ preprocessing/generate_dgl_graph.py
@@ -1,20 +1,19 @@
 import sys
-import numpy as np
 import pandas as pd
 import torch
 from preprocessing.read_event_log import GenerateTrace
-from collections import OrderedDict
-from sklearn.preprocessing import OneHotEncoder
 import pickle
-from utility import log_config as lc
 import dgl
 from sklearn import preprocessing
 from gensim.models import Word2Vec
-from sklearn.preprocessing import MinMaxScaler
 from Orange.data.pandas_compat import table_from_frame
 import Orange
 from dgl import AddReverse
-import sqlite3
+import h5py
+from pm4py.objects.log.importer.xes import importer as xes_importer
+import numpy as np
+from pm4py.objects.conversion.log import converter as xes_converter
+import os
 
 def replace_char(ele):
     ele = ele.replace(' ', '')
@@ -69,6 +68,25 @@ def gen_flow(id):
 
     return remove_dup, enc
 
+
+def create_triangular_matrix(columns):
+    # Convert set to sorted list for consistent indexing
+    cols = list(columns)
+    n = len(cols)
+
+    # Create empty matrix filled with zeros
+    list_relation = []
+    # Fill upper triangular part
+    for i in range(n):
+        for j in range(i, n):
+            # Create pair of column names
+            if cols[i] == cols[j]:
+                pair = (cols[i], 'follow', cols[j])
+            else:
+                pair = (cols[i], 'has', cols[j])
+            list_relation.append(pair)
+    return list_relation
+
 def gen_edge_weigts(list1, list2):
     combined_tuples = list(zip(list1, list2))
     tuple_counts = {}
@@ -109,20 +127,24 @@ def unique_edge(list1, list2):
     return unique_tuples
 
 
-def build_list_graphs(dict_view, dict_enc, mean, c):
+def build_list_graphs(dict_view, dict_y, dict_enc, mean, c, event_attributes, case_attributes, relation):
+
+
+    with h5py.File(c, "w") as f:
         k = 0
-        while k<len(dict_view['activity']):
+        list_graphs = []
+        while k<len(dict_view['concept:name']):
                 list_node = {}
                 list_node_comp = {}
                 list_node_feature = {}
                 dgl_canonical_edge = {}
                 weight_node_follow_node = {}
-                for v in lc.dict_log[log_name]['event_attr']:
+                for v in event_attributes:#lc.dict_log[log_name]['event_attr']:
                     list_node[v], list_node_comp[v] = gen_flow(dict_view[v][k])
                     list_node_feature[v] = apply_w2v(list_node[v], dict_enc[v], mean) #W2W
 
                 list_att_trace = []
-                for v in lc.dict_log[log_name]['trace_attr_cat']:
+                for v in case_attributes:#lc.dict_log[log_name]['trace_attr_cat']:
                     embed_vector = dict_enc[v].get(replace_char(dict_view[v][k][0]))
                     if embed_vector is not None:
                         res = embed_vector
@@ -130,13 +152,13 @@ def build_list_graphs(dict_view, dict_enc, mean, c):
                         res = np.zeros(shape=(mean,))
                     list_att_trace.append(res)
 
-                for v in lc.dict_log[log_name]['trace_attr_num']:
-                    list_att_trace.append(np.array(dict_view[v][k][0]).reshape(1))
+                #for v in lc.dict_log[log_name]['trace_attr_num']:
+                #    list_att_trace.append(np.array(dict_view[v][k][0]).reshape(1))
                 if list_att_trace !=[]:
                     list_node_comp['trace_att'] = [0]
                     list_node_feature['trace_att'] = np.array([np.concatenate(list_att_trace)])
 
-                for rel in lc.dict_log[log_name]['relation']:
+                for rel in relation:#lc.dict_log[log_name]['relation']:
                     if rel[1] == 'follow':
                         edge_res = np.array([[list_node_comp[rel[0]][i], list_node_comp[rel[0]][i + 1]] for i in range(len(list_node_comp[rel[0]]) - 1)])
                     elif rel[1] == 'has_ta':
@@ -153,13 +175,15 @@ def build_list_graphs(dict_view, dict_enc, mean, c):
 
 
                 for nn in list_node_feature:
-                    hetero_graph.nodes[nn].data[nn] = torch.tensor(list_node_feature[nn], dtype=torch.float)
+                        hetero_graph.nodes[nn].data[nn] = torch.tensor(list_node_feature[nn], dtype=torch.float)
 
                 for rel in weight_node_follow_node:
                     hetero_graph.edata['h'] = {rel:torch.tensor(weight_node_follow_node[rel])}
                 transform = AddReverse(copy_edata=True)
                 new_g = transform(hetero_graph)
-                insert_graph(new_g, c)
+                list_graphs.append(new_g)
+                pickled_graph = pickle.dumps({'graph':new_g, 'label':dict_y[k]})
+                f.create_dataset(f"array_{k}", data=np.void(pickled_graph))
                 k = k + 1
 
 
@@ -177,7 +201,7 @@ def equifreq(view_train, view_test, n_bin):
         return view_train, view_test
 
 def add_time_column(group):
-        timestamp_col = 'timestamp'
+        timestamp_col = 'time:timestamp'
         group = group.sort_values(timestamp_col, ascending=True)
         # end_date = group[timestamp_col].iloc[-1]
         start_date = group[timestamp_col].iloc[0]
@@ -193,46 +217,56 @@ def add_time_column(group):
 
         return group
 
-def insert_graph(graph, c):
-            serialized_graph = pickle.dumps(graph)
-            c.execute('''INSERT INTO graphs (graph) VALUES (?)''', (serialized_graph,))
-            conn.commit()
-
-
 if __name__ == '__main__':
 
         log_name = sys.argv[1]
 
-        print('log-->',log_name)
+        # Load the XES log
+        log = xes_importer.apply('fold/'+log_name+'.xes')
 
-        pm = GenerateTrace(log_name)
-        log = pm.import_log(log_name)
-        mean = 100
+        # Extract case attributes
+        case_attributes = []
+        for trace in log:
+            for attr in trace.attributes.keys():
+                case_attributes.append('case:'+attr)
 
-        log = log.fillna('unk')
-        log_train, log_test = pm.generate_split(log)
+        case_attributes = list(np.unique(case_attributes))
 
-        log_train['timestamp'] = pd.to_datetime(log_train['timestamp'])
-        log_test['timestamp'] = pd.to_datetime(log_test['timestamp'])
+        # Extract event attributes
+        event_attributes = []
+        for trace in log:
+            for event in trace:
+                for attr in event.keys():
+                    event_attributes.append(attr)
 
-        log_train = log_train.groupby('case', group_keys=False).apply(add_time_column)
-        log_test = log_test.groupby('case', group_keys=False).apply(add_time_column)
-        log_train = log_train.reset_index(drop=True)
-        log_test = log_test.reset_index(drop=True)
+        event_attributes = list(np.unique(event_attributes))
+
+        list_e_a = []
+        for i in list(event_attributes):
+            print('Event Attribute --> {}'.format(i))
+            print('Do you want to keep the feature? yes/no')
+            x = input()
+            if x == 'yes':
+                list_e_a.append(i)
+
+        list_e_a.remove('time:timestamp')
+        list_e_a.append('timesincecasestart')
+
+        list_c_a = []
+        for i in list(case_attributes):
+            if i != 'case:concept:name':
+                print('Case Attribute --> {}'.format(i))
+                print('Do you want to keep the feature? yes/no')
+                x = input()
+                if x == 'yes':
+                    list_c_a.append(i)
 
 
-        if log_name != 'helpdesk2' and log_name != 'sp2020':
-            num_act = list(set(list(log_train['activity'].unique()) + list(log_test['activity'].unique())))
-            num_res = list(set(list(log_train['resource'].unique()) + list(log_test['resource'].unique())))
-            n_bin = (len(num_act) + len(num_res)) // 2
-
-            log_train['resource'] = log_train['resource'].astype(str)
-            log_test['resource'] = log_test['resource'].astype(str)
-        else:
-            num_act = list(set(list(log_train['activity'].unique()) + list(log_test['activity'].unique())))
-            n_bin = len(num_act)
-
-        log_train['timesincecasestart'], log_test['timesincecasestart'] = equifreq(log_train['timesincecasestart'], log_test['timesincecasestart'], n_bin)
+        result = create_triangular_matrix(list_e_a)
+        result2 = []
+        if len(list_c_a) > 0:
+            for x in list_e_a:
+                    result2.append((x, 'has_ta', 'trace_att'))
 
         dict_card = {}
         dict_view_train = {}
@@ -241,7 +275,46 @@ if __name__ == '__main__':
         dict_view_test_y = {}
         dict_enc = {}
 
-        for attr in lc.dict_log[log_name]['event_attr']:
+        relation = result + result2
+        print('Event Attribute --> {}'.format(list_e_a))
+        print('Case Attribute --> {}'.format(list_c_a))
+        print('Relations --> {}'.format(relation))
+
+        print('log-->', log_name)
+
+        pm = GenerateTrace(log_name)
+        log = xes_converter.apply(log, variant=xes_converter.Variants.TO_DATA_FRAME)
+
+        mean = 100
+
+        log = log.fillna('unk')
+        log_train, log_test = pm.generate_split(log)
+
+        log_train['time:timestamp'] = pd.to_datetime(log_train['time:timestamp'])
+        log_test['time:timestamp'] = pd.to_datetime(log_test['time:timestamp'])
+
+        log_train = log_train.groupby('case:concept:name', group_keys=False).apply(add_time_column)
+        log_test = log_test.groupby('case:concept:name', group_keys=False).apply(add_time_column)
+        log_train = log_train.reset_index(drop=True)
+        log_test = log_test.reset_index(drop=True)
+
+        if 'org:resource' in list_e_a:
+            num_act = list(set(list(log_train['concept:name'].unique()) + list(log_test['concept:name'].unique())))
+            num_res = list(set(list(log_train['org:resource'].unique()) + list(log_test['org:resource'].unique())))
+            n_bin = (len(num_act) + len(num_res)) // 2
+            log_train['org:resource'] = log_train['org:resource'].astype(str)
+            log_test['org:resource'] = log_test['org:resource'].astype(str)
+        else:
+            num_act = list(set(list(log_train['concept:name'].unique()) + list(log_test['concept:name'].unique())))
+            n_bin = len(num_act)
+
+        if not os.path.exists("w2v/" + log_name):
+            os.mkdir("w2v/" + log_name)
+
+        for attr in list_e_a:
+            is_numeric = pd.to_numeric(log_train[attr], errors='coerce').notnull().all()
+            if is_numeric:
+                log_train[attr], log_test[attr] = equifreq(log_train[attr], log_test[attr], n_bin)
             node_encoder = preprocessing.OneHotEncoder()
             dict_card[attr] = list(set(pd.concat([log_train[attr],log_test[attr]])))
             dict_card[attr].insert(0, 'START')
@@ -253,15 +326,12 @@ if __name__ == '__main__':
             output.close()
             dict_att = [[a] for a in dict_card[attr]]
 
-        for attr in lc.dict_log[log_name]['trace_attr_num']:
-            scaler = MinMaxScaler()
-            log_train[attr] = scaler.fit_transform(log_train[attr].values.reshape(len(log_train),-1))
-            log_test[attr] = scaler.transform(log_test[attr].values.reshape(len(log_test),-1))
-            dict_view_train[attr] = pm.get_sequence_num(pm.generate_prefix_trace(log=log_train, view=attr))
-            dict_view_test[attr] = pm.get_sequence_num(pm.generate_prefix_trace(log=log_test, view=attr))
-
-
-        for attr in lc.dict_log[log_name]['trace_attr_cat']:
+        for attr in list_c_a:
+            is_numeric = pd.to_numeric(log_train[attr], errors='coerce').notnull().all()
+            print(attr, is_numeric)
+            if is_numeric:
+                print('numerical column--->', attr)
+                log_train[attr], log_test[attr] = equifreq(log_train[attr], log_test[attr], n_bin)
             dict_card[attr] = list(set(pd.concat([log_train[attr], log_test[attr]])))
             dict_view_train[attr] = pm.get_sequence_num(pm.generate_prefix_trace(log=log_train, view=attr))
             dict_view_test[attr] = pm.get_sequence_num(pm.generate_prefix_trace(log=log_test, view=attr))
@@ -271,46 +341,28 @@ if __name__ == '__main__':
             output.close()
             dict_att = [[a] for a in dict_card[attr]]
 
-        conn = sqlite3.connect('heterographs_tracenode/' + log_name + '_train.db')
-        c = conn.cursor()
-
-        # Create table if not exists
-        c.execute('''CREATE TABLE IF NOT EXISTS graphs (id INTEGER PRIMARY KEY, graph BLOB)''')
-
-        build_list_graphs(dict_view_train, dict_enc, mean, c)
-        print('end_train')
-        conn.close()
-
-        conn = sqlite3.connect('heterographs_tracenode/' + log_name + '_test.db')
-        c = conn.cursor()
-
-        # Create table if not exists
-        c.execute('''CREATE TABLE IF NOT EXISTS graphs (id INTEGER PRIMARY KEY, graph BLOB)''')
-
-        build_list_graphs(dict_view_test, dict_enc, mean, c)
-
         label_encoder = preprocessing.LabelEncoder()
-        dict_card['activity'].remove('START')
+        dict_card['concept:name'].remove('START')
 
-        integer_encoded = label_encoder.fit_transform(dict_card['activity'])
+        integer_encoded = label_encoder.fit_transform(dict_card['concept:name'])
         integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
 
-        onehot_encoder = preprocessing.OneHotEncoder(sparse=False)
+        onehot_encoder = preprocessing.OneHotEncoder(sparse_output=False)
         onehot_encoder.fit(integer_encoded)
         onehot_encoded = onehot_encoder.transform(integer_encoded)
 
-        train_integer_encoded = label_encoder.transform(dict_view_train_y['activity']).reshape(-1, 1)
+        train_integer_encoded = label_encoder.transform(dict_view_train_y['concept:name']).reshape(-1, 1)
         train_onehot_encoded = onehot_encoder.transform(train_integer_encoded)
         Y_train = np.asarray(train_onehot_encoded)
 
-        test_integer_encoded = label_encoder.transform(dict_view_test_y['activity']).reshape(-1, 1)
+        test_integer_encoded = label_encoder.transform(dict_view_test_y['concept:name']).reshape(-1, 1)
         test_onehot_encoded = onehot_encoder.transform(test_integer_encoded)
         Y_test = np.asarray(test_onehot_encoded)
         Y_test_int = np.asarray(test_integer_encoded)
 
-        with open('heterographs_tracenode/'+log_name+'_ytrain.pickle', 'wb') as handle:
-            pickle.dump(Y_train, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        with open('heterographs_tracenode/'+log_name+'_ytest.pickle', 'wb') as handle:
-            pickle.dump(Y_test, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        with open('heterographs_tracenode/'+log_name+'_ytestint.pickle', 'wb') as handle:
-            pickle.dump(Y_test_int, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        build_list_graphs(dict_view_train, Y_train, dict_enc, mean, 'heterographs_tracenode/' + log_name + '_train.db', list_e_a, list_c_a, relation)
+        print('end train generation')
+
+        build_list_graphs(dict_view_test, Y_test, dict_enc, mean, 'heterographs_tracenode/' + log_name + '_test.db', list_e_a, list_c_a, relation)
+        print('end test generation')
